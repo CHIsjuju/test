bde4b8161f5b3cd0679502ab29e19e1183d39b9e
CHIsjuju <fiona10133@gmail.com>
2024-07-31 21:09:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World!</p>
+        <p>React to GitHub </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
